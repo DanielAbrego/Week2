ac3185dc6b47f0588441bbdad56489279d880d22
danielabrego <abregomo@sheridancollege.ca>
2022-05-26 14:12:52 -0400

    Student Project

---
+++ src/StudentDetailsPrint.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author dabre
+ */
+public class StudentDetailsPrint {
+    public static void main(String[] args) {
+	
+	Student s1 = new Student ();
+	s1.setName("john");
+	s1.setAge(19);
+	Student s2 = new Student ();
+	s2.setName("Peter");
+	s2.setAge("19");
+	Student s3 = new Student ();
+	s3.setName("Jane");
+	s3.setAge("19")
+	list[0]=s1;
+	list[1]=s2;
+	list[2]=s3;
+	for(int i=0;i<list.length 
+}
